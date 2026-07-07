c42a75a28400fca9eb9242c8d93de57169d97458
ievag7 <gievoote@gmail.com>
2026-07-08 01:01:39 +0300

    Select random product and get product name

--- tests/findBugs.spec.ts
+++ tests/findBugs.spec.ts
@@ -1,7 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-test ('open Academybugs website', async ({page}) => {
+test ('Add product to cart and verify details', async ({page}) => {
     await page.goto('https://academybugs.com/find-bugs/');
     await expect(page)
-        .toHaveTitle(/Find/);
+        .toHaveTitle(/Find Bugs/);
+
+    const product = page.locator('.ec_image_link_cover').filter({hasText: /.+/ });
+
+    const count = await product.count(); 
+    
+    const randomIndex = Math.floor(Math.random() * count); 
+    
+    const randomProduct = product.nth(randomIndex); 
+    
+    const productName = await randomProduct.textContent(); 
+    
+    console.log('Product name:', productName);
+
+   
+
+   
+
 });
