ddd0db0ea1926ce6381535edb765bb5ca4f31267
ievag7 <gievoote@gmail.com>
2026-07-08 12:45:45 +0300

    Comments added

--- tests/findBugs.spec.ts
+++ tests/findBugs.spec.ts
@@ -8,20 +8,29 @@ test ('Add product to cart and verify details', async ({page}) => {
     const findBugsPage = new FindBugsPage(page);
     const productSelection = new ProductSelection(page);
     const cartPage = new CartPage(page);
-
-    await findBugsPage.goto();
+ 
+    //Open https://academybugs.com/find-bugs/ URL
+    await findBugsPage.goto();      
     await findBugsPage.acceptCookies();
 
+    //Add any product to the cart
     const product = await productSelection.getRandomVisibleProduct();
 
     const { response, json, productName, productPrice } =
         await productSelection.addProductToCart(product);
 
+
+    //Verify API response status
     expect(response.ok()).toBeTruthy();
+
+    //Verify the product name in response matches the UI
     expect(json[0].title.toUpperCase()).toContain(productName.toUpperCase());
 
+    //Open cart
     await productSelection.openCart();
 
+
+    //Verify the price matches with one on all products page
      await cartPage.verifyProduct(productName, productPrice);
 });
     
