ac329fd784e5ef3697a7721d7548eae04705572e
ievag7 <gievoote@gmail.com>
2026-07-08 10:56:27 +0300

    Adding product to cart and verifying details

--- tests/findBugs.spec.ts
+++ tests/findBugs.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test ('Add product to cart and verify details', async ({page}) => {
     await page.goto('https://academybugs.com/find-bugs/');
 
-        const acceptCookies = page.getByRole('button', { name: 'Accept cookies' });
+const acceptCookies = page.getByRole('button', { name: 'Accept cookies' });
 
         if (await acceptCookies.isVisible()) {
             await acceptCookies.click();
@@ -11,7 +11,8 @@ test ('Add product to cart and verify details', async ({page}) => {
 
         await expect(page).toHaveTitle(/Find Bugs/);
 
-const products = page.locator('.ec_product_li').filter({ has: page.locator('.ec_price_type1') });
+const products = page.locator('.ec_product_li')
+.filter({ has: page.locator('.ec_price_type1') });
 
 const visibleProducts = [];
 
@@ -22,8 +23,6 @@ for (let i = 0; i < count; i++) {
         visibleProducts.push(products.nth(i));
     }
 }
-
-console.log('Visible products:', visibleProducts.length);
 
 const randomProduct = visibleProducts[
     Math.floor(Math.random() * visibleProducts.length)
@@ -37,40 +36,55 @@ const productPrice = await randomProduct
     .locator('.ec_price_type1')
     .innerText();
 
-console.log('Product name:', productName);
-console.log('Product price:', productPrice);
-
-
-    await randomProduct.click();
-
-    await expect(page).toHaveURL(/store/);
-    
-    const addToCartButton = page.getByRole('button', { name: 'ADD TO CART' });
+const addToCartButton = randomProduct
+    .locator('.ec_product_addtocart a:visible')
+    .filter({ hasNotText: 'Select Options' })
+    .first();
 
     await expect(addToCartButton).toBeVisible();
 
-    const responsePromise = page.waitForResponse(response =>
+    
+
+const responsePromise = page.waitForResponse(response =>
         response.url().includes('admin-ajax.php') &&
         response.request().method() === 'POST'
     );
-    
-    await addToCartButton.click();
-    
-    const response = await responsePromise;
 
-    const responseBody = await response.text();
-    
+    await addToCartButton.click();
+
+   
+const response = await responsePromise;
+
     expect(response.ok()).toBeTruthy();
 
-    const cartProduct = page.getByRole('link', {name: productName}).first();
+const responseBody = await response.text();
 
-    await expect(cartProduct).toBeVisible();
+    expect(responseBody.toLocaleUpperCase())
+        .toContain(productName.toLocaleUpperCase());
 
-  
+const viewCartButton = page.getByRole('link', { name: 'View Cart' });
+
+    await expect(viewCartButton).toBeVisible();
+
+    await viewCartButton.click();
+
+    await expect(page).toHaveURL(/my-cart/);
+
+    
+const cartProductName = page
+        .getByRole('link', {name: productName})
+        .first();
 
 
-   
+    await expect(cartProductName).toBeVisible();
 
-   
 
+const cartProductPrice = await page
+        .locator('.ec_cartitem_price')
+        .innerText();
+
+
+    expect(cartProductPrice.trim())
+        .toBe(productPrice.trim());
+    
 });
